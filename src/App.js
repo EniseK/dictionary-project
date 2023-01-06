0db68745a8e7c5ba4d2e934aec0af2418585e81a
EniseK <enisekizilaslan@gmail.com>
2023-01-06 12:48:00 +0100

    Added the Dictionary.js and css components. Built the main structures of the search engine using necessary functions.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header"></header>
-			Hello
+			<div className="container">
+				<header className="App-header">Dictionary App</header>
+				<main>
+					<Dictionary />
+				</main>
+
+				<footer className="App-footer">
+					{" "}
+					<small>
+						Coded by Enise Kizilaslan, open-sourced by{" "}
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://github.com/EniseK/dictionary-project"
+						>
+							GitHub,
+						</a>{" "}
+						and hosted on{" "}
+						<a
+							href="https://app.netlify.com/sites/dictionary-app-project/overview"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Netlify.
+						</a>
+					</small>
+				</footer>
+			</div>
 		</div>
 	);
 }
